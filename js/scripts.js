2e2413abc24db3f58375c985d029cb359b54a1b2
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2021-09-11 13:55:14 -0300

    Criando a lógica do jogo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,46 @@
-console.log("Teste");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let button = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+// contador de jogadas
+
+let player1 = 0;
+let player2 = 0;
+
+// adicionando evento de click aos boxes
+
+for(let i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener("click", function(){
+
+        let el;
+       
+        if(player1 == player2){
+            // x
+            el = x;
+        }else{
+            // o
+            el = o;
+        }
+
+
+        // verifica se já tem x ou o
+       if(this.childNodes.length == 0){
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            // computar jogada
+
+            if(player1 == player2){
+                player1++;
+            }else{
+                player2++;
+            }
+        }
+
+    });
+}
